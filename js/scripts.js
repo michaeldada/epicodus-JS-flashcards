b2851d88a4a069a24ed1e5e823fbb65652fe5d68
Anna Lawley and Cassy Culkins <alawley1@gmail.com>
2016-01-11 15:46:35 -0800

    add toggle functionality to flashcards.

---
+++ js/scripts.js
@@ -0,0 +1,146 @@
+jQuery(document).ready(function() {
+  $("#q1").click(function() {
+    $("#a1").toggle();
+    $("#q1").toggle();
+  });
+  $("#a1").click(function() {
+    $("#a1").toggle();
+    $("#q1").toggle();
+  });
+  $("#q2").click(function() {
+    $("#a2").toggle();
+    $("#q2").toggle();
+  });
+  $("#a2").click(function() {
+    $("#a2").toggle();
+    $("#q2").toggle();
+  });
+  $("#q3").click(function() {
+    $("#a3").toggle();
+    $("#q3").toggle();
+  });
+  $("#a3").click(function() {
+    $("#a3").toggle();
+    $("#q3").toggle();
+  });
+  $("#q4").click(function() {
+    $("#a4").toggle();
+    $("#q4").toggle();
+  });
+  $("#a4").click(function() {
+    $("#a4").toggle();
+    $("#q4").toggle();
+  });
+  $("#q5").click(function() {
+    $("#a5").toggle();
+    $("#q5").toggle();
+  });
+  $("#a5").click(function() {
+    $("#a5").toggle();
+    $("#q5").toggle();
+  });
+  $("#q6").click(function() {
+    $("#a6").toggle();
+    $("#q6").toggle();
+  });
+  $("#a6").click(function() {
+    $("#a6").toggle();
+    $("#q6").toggle();
+  });
+  $("#q7").click(function() {
+    $("#a7").toggle();
+    $("#q7").toggle();
+  });
+  $("#a7").click(function() {
+    $("#a7").toggle();
+    $("#q7").toggle();
+  });
+  $("#q8").click(function() {
+    $("#a8").toggle();
+    $("#q8").toggle();
+  });
+  $("#a8").click(function() {
+    $("#a8").toggle();
+    $("#q8").toggle();
+  });
+  $("#q9").click(function() {
+    $("#a9").toggle();
+    $("#q9").toggle();
+  });
+  $("#a9").click(function() {
+    $("#a9").toggle();
+    $("#q9").toggle();
+  });
+  $("#q10").click(function() {
+    $("#a10").toggle();
+    $("#q10").toggle();
+  });
+  $("#a10").click(function() {
+    $("#a10").toggle();
+    $("#q10").toggle();
+  });
+  $("#q11").click(function() {
+    $("#a11").toggle();
+    $("#q11").toggle();
+  });
+  $("#a11").click(function() {
+    $("#a11").toggle();
+    $("#q11").toggle();
+  });
+  $("#q12").click(function() {
+    $("#a12").toggle();
+    $("#q12").toggle();
+  });
+  $("#a12").click(function() {
+    $("#a12").toggle();
+    $("#q12").toggle();
+  });
+  $("#q13").click(function() {
+    $("#a13").toggle();
+    $("#q13").toggle();
+  });
+  $("#a13").click(function() {
+    $("#a13").toggle();
+    $("#q13").toggle();
+  });
+  $("#q14").click(function() {
+    $("#a14").toggle();
+    $("#q14").toggle();
+  });
+  $("#a14").click(function() {
+    $("#a14").toggle();
+    $("#q14").toggle();
+  });
+  $("#q15").click(function() {
+    $("#a15").toggle();
+    $("#q15").toggle();
+  });
+  $("#a15").click(function() {
+    $("#a15").toggle();
+    $("#q15").toggle();
+  });
+  $("#q16").click(function() {
+    $("#a16").toggle();
+    $("#q16").toggle();
+  });
+  $("#a16").click(function() {
+    $("#a16").toggle();
+    $("#q16").toggle();
+  });
+  $("#q17").click(function() {
+    $("#a17").toggle();
+    $("#q17").toggle();
+  });
+  $("#a17").click(function() {
+    $("#a17").toggle();
+    $("#q17").toggle();
+  });
+  $("#q18").click(function() {
+    $("#a18").toggle();
+    $("#q18").toggle();
+  });
+  $("#a18").click(function() {
+    $("#a18").toggle();
+    $("#q18").toggle();
+  });
+});
